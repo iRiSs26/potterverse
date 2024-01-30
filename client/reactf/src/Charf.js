a4288279f1233094480fc5b60b384bba2cac27b6
Anjali <anjali.12114710@lpu.in>
2024-01-30 22:18:51 +0530

    Home page updated and character info page added

--- client/reactf/src/Charf.js
+++ client/reactf/src/Charf.js
@@ -1,16 +1,106 @@
 import React from 'react';
 import './Charf.css';
-import logo from './harry_potter-1.jpg';
-import log from './hp.jpg';
+import './Charfglobal.css';
+// import logo from './harry_potter-1.jpg';
+// import log from './hp.jpg';
+import img1 from './harry_potter-1.jpg';
+import img2 from './hp.jpg';
 
 function Charf() {
   return (
     <div>
         <div className='back'>
-        <img src={logo} className='bg-name' ></img>
-        <div>
+        {/* <img src={logo} className='bg-name' ></img> */}
+        {/* <div>
             <img src={log} className='bc-name'></img>
-        </div>
+        </div> */}
+        <div className="desktop-1">
+      <main className="inner-frame">
+        <div className="patronusstag">PATRONUS:STAG</div>
+        <section className="child-frame">
+          <img className="wp7316242-1-icon" alt="" src={img1} />
+          <div className="hp-frame">
+            <div className="second-shape-parent">
+              <div className="second-shape">
+                <div className="third-shape">
+                  <div className="fourth-shape">
+                    <div className="fifth-shape" />
+                    <div className="albus-severus-you">
+                      "Albus Severus, you were named for two headmasters of
+                      Hogwarts. One of them was a Slytherin and he was probably
+                      the bravest man I ever knew."
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="seventh-frame">
+                <div className="eighth-frame">
+                  <div className="ninth-rectangle" />
+                  <div className="harry-james56-potter-container">
+                    <p className="harry-james56-potter">
+                      Harry James[56] Potter (b. 31 July 1980)[1] was an English
+                      half-blood[2] wizard, and one of the most famous wizards
+                      of modern times. The only child and son of James and Lily
+                      Potter (née Evans), Harry's birth was overshadowed by a
+                      prophecy, naming either himself or Neville Longbottom as
+                      the one with the power to vanquish Lord Voldemort, the
+                      most powerful and feared Dark wizard in the world. After
+                      half of the prophecy was reported to Voldemort, courtesy
+                      of Severus Snape, Harry was chosen as the target due to
+                      his many similarities with the Dark Lord. This in turn
+                      caused the Potter family to go into hiding.
+                    </p>
+                    <p className="harry-was-also">
+                      Harry was also noted for being the only known Master of
+                      Death, having united the three Deathly Hallows at the mere
+                      age of seventeen.
+                    </p>
+                    <p className="after-the-war">
+                      After the war, Harry became an Auror and helped reform and
+                      revolutionise the Ministry of Magic.At some point, he
+                      married Ginny, with whom he had three children: James
+                      Sirius, Albus Severus, and Lily Luna. He was also named
+                      the godfather of Edward "Teddy" Remus Lupin.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="eleventh-frame">
+            <img
+              className="twelfth-rectangle-icon"
+              loading="eager"
+              alt=""
+              src={img2}
+            />
+            <div className="thirteenth-frame">
+              <div className="fourteenth-frame">
+                <div className="fifteenth-frame">
+                  <div className="sixteenth-rectangle">
+                    <div className="sixteenth-rectangle-child" />
+                    <div className="harry-potter">
+                      <span>{`     `}</span>
+                      <span className="harry-potter1"> HARRY POTTER</span>
+                    </div>
+                  </div>
+                  <img
+                    className="fifteenth-frame-child"
+                    loading="eager"
+                    alt=""
+                    src="/rectangle-3.svg"
+                  />
+                </div>
+                <div className="blood-statushalf">BLOOD-STATUS:HALF</div>
+              </div>
+              <div className="frame-group" />
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
+
+        
         </div>
         
       
