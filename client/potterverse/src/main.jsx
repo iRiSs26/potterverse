33b14b652a0706ced9df62269135e9d3bbe1931c
Anjali <anjali.12114710@lpu.in>
2024-02-03 02:23:55 +0530

    new page with welcome message

--- client/potterverse/src/main.jsx
+++ client/potterverse/src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Home from './Home.jsx'
+import New from './New.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home />
+    <New />
   </React.StrictMode>,
 )
